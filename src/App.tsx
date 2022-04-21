fe4e14b8913e8a8f2d90205e79898ec01d134eba
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-21 14:02:00 +0900

    リダイレクトの設定をルーティングファイルに記述、チラつきを制御

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import { Store } from './store'
 
 const App: React.FC = () => {
-  const navigate = useNavigate()
-  const { state } = useContext(Store)
-
-  useEffect(() => {
-    if (!state.id) navigate('/login')
-  }, [navigate, state.id])
-
   return <Outlet />
 }
 
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
+import { Store } from '../store'
 import App from '../App'
 import Login from '../pages/Login'
 import NotFound from '../pages/NotFound'
@@ -9,13 +11,20 @@ import Top from '../pages/Top'
 import User from '../pages/User'
 
 const Router: React.FC = () => {
+  const { state } = useContext(Store)
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={state.id ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={state.id ? <Navigate to="/" /> : <Register />}
+        />
 
-        <Route element={<App />}>
+        <Route element={state.id ? <App /> : <Navigate to="/login" />}>
           <Route path="/" element={<Top />} />
           <Route path="/users" element={<User />} />
           <Route path="/posts" element={<Post />} />
